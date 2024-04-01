e46920579518e5e331189c52c1f964dcf82a1108
Aelkoulali <abdellah.elkoulali@gmx.fr>
2024-04-01 16:50:23 +0200

    Add Delete Song Function

--- script.js
+++ script.js
@@ -145,6 +145,42 @@ const allSongs = [
     setPlayerDisplay();
     setPlayButtonAccessibleText();
   };
+  // Fuction To Delete Song
+  const deleteSong = (id) => {
+    if (userData?.currentSong?.id === id) {
+      userData.currentSong = null;
+      userData.songCurrentTime = 0;
+  
+      pauseSong();
+      setPlayerDisplay();
+    }
+  
+    userData.songs = userData?.songs.filter((song) => song.id !== id);
+    renderSongs(userData?.songs); 
+    highlightCurrentSong(); 
+    setPlayButtonAccessibleText(); 
+  
+    if (userData?.songs.length === 0) {
+      const resetButton = document.createElement("button");
+      const resetText = document.createTextNode("Reset Playlist");
+  
+      resetButton.id = "reset";
+      resetButton.ariaLabel = "Reset playlist";
+      resetButton.appendChild(resetText);
+      playlistSongs.appendChild(resetButton);
+  
+      resetButton.addEventListener("click", () => {
+        userData.songs = [...allSongs];
+  
+        renderSongs(sortSongs()); 
+        setPlayButtonAccessibleText();
+        resetButton.remove();
+      });
+  
+    }
+  
+  };
+  
   // Function To Set Player Display
   
   const setPlayerDisplay = () => {
